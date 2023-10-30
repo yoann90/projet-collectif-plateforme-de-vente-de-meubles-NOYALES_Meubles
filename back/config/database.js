cdead278419c2ebe814515e62b51db12c3c85c11
Aisseta <diaw.aisseta@gmail.com>
2023-10-30 16:40:04 +0100

    connexion db

---
+++ back/config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const {connect} = require('mongoose');
+
+function dbConnexion() {
+    connect('mongodb+srv://diawaisseta:ProjetMeubles@cluster0.e0brxpv.mongodb.net/Projets-Meubles')
+    .then(() => console.log('Connexion ok'))
+    .catch(error => console.log(error))
+}
+
+module.exports = dbConnexion
--- back/server.js
+++ back/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const routes = require("./routes/routes.js")
+const routes = require("./routes/routes.js");
+const db =require('./config/database');
 
-
+db()
 app.use(express.json())
 app.use(routes)
 app.listen(port,() => console.log("serveur ok "))
