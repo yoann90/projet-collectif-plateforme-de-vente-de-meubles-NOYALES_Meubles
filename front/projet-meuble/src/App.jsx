cbeda92229f8277dc83365628225334696dbd3d9
LalowDavi <laura.davignon@hotmail.com>
2023-10-31 14:52:38 +0100

    delete bootstrap2

--- front/projet-meuble/src/App.jsx
+++ front/projet-meuble/src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Navbar from "./PagePrincipal/PagePrincipal";
 import { BrowseRouter, Routes, Route } from "react-router-dom";
 
-import "bootstrap/dist/css/bootstrap.min.css"; // Assurez-vous d'importer le fichier CSS Bootstrap
+
 
 function App() {
   return (
