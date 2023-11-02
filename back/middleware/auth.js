cec287b96f63e745db902b77f4397c72f7d07977
Lachignol <110435478+Lachignol@users.noreply.github.com>
2023-11-02 20:37:27 +0100

    authentificationaveccorrespondancebase

--- back/middleware/auth.js
+++ back/middleware/auth.js
@@ -1,23 +1,28 @@
 const jwt = require("jsonwebtoken");
-const User= require("../models/usermodel");
+const User = require("../models/usermodel");
 require("dotenv").config()
 const path = require("path")
 
 
-const authenticateJWT = (req, res, next) => {
+const authenticateJWT = async(req, res, next) => {
     const authHeader = req.headers.authorization;
 
     if (authHeader) {
         const token = authHeader.split(' ')[1];
 
-        jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+        jwt.verify(token, process.env.SECRET_KEY, async(err, user) => {
             if (err) {
                 return res.sendStatus(403);
             }
-
             req.user = user;
-            console.log(user)
+            console.log(user.id)
+            let userProfil = await User.findById({_id: user.id})
+            if(userProfil){
+            console.log(userProfil)
             next();
+        }else{
+        res.status(401).json({msg:"utilisateur introuvable"})
+        }
         });
     } else {
         res.sendStatus(401);
