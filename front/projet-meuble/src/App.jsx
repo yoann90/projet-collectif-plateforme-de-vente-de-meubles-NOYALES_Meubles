c5f4ccfaf7b1b6810394e01d7cee3b15bf552f4d
LalowDavi <laura.davignon@hotmail.com>
2023-11-03 15:47:18 +0100

    page de connexion BG

--- front/projet-meuble/src/App.jsx
+++ front/projet-meuble/src/App.jsx
@@ -3,34 +3,33 @@ import "./App.css";
 import {Navbar,Header,Produit,AllProduits,Cart,ScrollToTopButton} from "./PagePrincipal/PagePrincipal";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Furniture from "./PageDetail/components/PageDetail";
+import UserConnexion from './PageConnexion/page_connexion';
 
 
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      {/* <Route path="/" element={<Navbar />} /> */}
-      {/* <Route path="/page detail" element={< page />} /> */}
-      </Routes>
-
-   
-    </BrowserRouter>
-    <Navbar />
-       {/* <Header />
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" element={<Navbar />} /> */}
+          {/* <Route path="/page detail" element={< page />} /> */}
+        </Routes>
+      </BrowserRouter>
+      <Navbar />
+      {/* <Header />
        <Produit /> */}
-     
-       <Header />
-       <Furniture />
 
-       <Produit />
-      <Cart />
-     
-      <AllProduits />
+      {/* <Header /> */}
+      <UserConnexion />
+      {/* <Furniture /> */}
+      {/* <Produit /> */}
+      {/* <Cart /> */}
+
+      {/* <AllProduits /> */}
       <ScrollToTopButton />
     </>
-  )
+  );
 }
 
 export default App;
